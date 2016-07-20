7bcb970b2f06b24000aafdc2390cbd0bb2d0e224
Andrew Hatfield <andrew.hatfield@cynosureservices.com>
2016-07-20 21:50:10 +1000

    more <BR>

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ print ("helo again!");
 print "This next bit shouldn't work straight away - we haven't attached a Red Hat Gluster Storage Volume yet!";
 
 ?>
+<br><br>
 
 <img src="/images/picture.jpg" alt="picture on a Red Hat Gluster Storage Volume">
 
