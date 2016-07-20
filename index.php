54c0cd17830ef30d399248bc4e767b9e88e120a2
Andrew Hatfield <andrew.hatfield@cynosureservices.com>
2016-07-20 21:09:56 +1000

    added a the picture on a volume

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 <h1>hello sweetie!</h1>
 <?php
 print ("helo again!");
+
+print "This next bit shouldn't work straight away - we haven't attached a Red Hat Gluster Storage Volume yet!";
+
 ?>
+
+<img src="/images/picture.jpg" alt="picture on a Red Hat Gluster Storage Volume">
+
 </BODY>
 </HTML>
